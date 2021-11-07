8bff95c5345789781a92306e18e6c46d10454b89
CollinEdward <manorengaard@icloud.com>
2021-11-07 13:44:07 +0100

    Fix device detection problem, + clean up code a little.

--- GUI Version/DosSonosGUI.py
+++ GUI Version/DosSonosGUI.py
@@ -9,16 +9,22 @@ root = Tk()
 root.title("Dos sonos GUI")
 root.geometry("600x800")
 
+device = soco.discovery.any_soco()
+
 def start_dos():
     while len(str(device)) != 0:
-        device = soco.discovery.any_soco()
-        print("Currently dossing device: " + device + " :")
-        device.
+        if device == None:
+            print("No device found on network")
+            print("Exiting test. .")
+            import os
+            os._exit()
+    else:
+        print("Currently dossing device: " + str(device) + " :")
     if len(str(device)) == 0:
         print("Check if the device is up and running on the network")
 
-button_start = Button(root, text="start dos attack", command=start_dos()).pack()
+button_start = Button(root, text="start dos attack", height=1, width=20, bg="grey", command=start_dos()).pack()
 
-button_exit = Button(root, text="EXIT ", command=exit).pack()
+button_exit = Button(root, text="EXIT ", height=1, width=20, bg="grey", command=exit).pack()
 
 root.mainloop()
